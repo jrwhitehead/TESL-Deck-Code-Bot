b169494a303b4c5fe3425eaa07e39e9f5aaba80f
jrwhitehead <1528969+jrwhitehead@users.noreply.github.com>
2019-11-04 14:45:23 +0000

    Update main.py

--- tesldeckcodebot/main.py
+++ tesldeckcodebot/main.py
@@ -1,7 +1,6 @@
 from bot import TESLDeckCodeBot
 import argparse
 
-print('deck-code-bot # Initialising...')
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='The Elder Scrolls: Legends Deck Code bot for Reddit.')
     # No default value to prevent accidental mayhem
@@ -9,7 +8,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    print('deck-code-bot # Started lurking in (/r/{})'.format(args.target_sub))
+    print('TESL-Deck-Code-bot # Started lurking in (/r/{})'.format(args.target_sub))
     bot = TESLDeckCodeBot(author='NotGooseFromTopGun', target_sub=args.target_sub)
     bot.start(batch_limit=10, buffer_size=1000)
     print('deck-code-bot # Stopped running.')
